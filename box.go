6d3e47b07f425db98da6a65ad3bb0088cecf14c7
yaotthaha <yaotthaha@foxmail.com>
2023-10-10 10:40:10 +0800

    Add reload support
    (cherry picked from commit https://github.com/rnetx/sing-box/commit/e98000c371095c1d5f7fcec4a02ffaaf22a68e47)

--- box.go
+++ box.go
@@ -38,6 +38,7 @@ type Box struct {
 	preServices1 map[string]adapter.Service
 	preServices2 map[string]adapter.Service
 	postServices map[string]adapter.Service
+	reloadChan   chan struct{}
 	done         chan struct{}
 }
 
@@ -50,6 +51,7 @@ type Options struct {
 
 func New(options Options) (*Box, error) {
 	createdAt := time.Now()
+	reloadChan := make(chan struct{}, 1)
 	ctx := options.Context
 	if ctx == nil {
 		ctx = context.Background()
@@ -93,6 +95,7 @@ func New(options Options) (*Box, error) {
 		common.PtrValueOrDefault(options.NTP),
 		options.Inbounds,
 		options.PlatformInterface,
+		reloadChan,
 	)
 	if err != nil {
 		return nil, E.Cause(err, "parse route options")
@@ -192,6 +195,7 @@ func New(options Options) (*Box, error) {
 		preServices1: preServices1,
 		preServices2: preServices2,
 		postServices: postServices,
+		reloadChan:   reloadChan,
 		done:         make(chan struct{}),
 	}, nil
 }
@@ -385,4 +389,8 @@ func (s *Box) Close() error {
 
 func (s *Box) Router() adapter.Router {
 	return s.router
+}
+
+func (s *Box) ReloadChan() <-chan struct{} {
+	return s.reloadChan
 }
